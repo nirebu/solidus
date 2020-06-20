5c78974b2175d78d41d6263db924c5b1d2fc9499
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2020-06-20 19:19:15 +0200

    Add special case for countries where the subregion data useful for shipping is nested in base subregions

--- core/db/default/spree/states.rb
+++ core/db/default/spree/states.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveRecord::Base.transaction do
-  Spree::Country.all.each do |country|
+  Spree::Country.where.not(iso: 'IT').each do |country|
     carmen_country = Carmen::Country.coded(country.iso)
     next unless carmen_country.subregions?
 
@@ -9,6 +9,18 @@ ActiveRecord::Base.transaction do
       Spree::State.where(abbr: subregion.code, country: country).first_or_create!(
         name: subregion.name
       )
+    end
+  end
+  Spree::Country.where(iso: 'IT').each do |country|
+    carmen_country_with_provinces = Carmen::Country.coded(country.iso)
+    next unless carmen_country_with_provinces.subregions?
+
+    carmen_country_with_provinces.subregions.each do |subregion|
+      subregion.subregions.each do |province|
+        Spree::State.where(abbr: province.code, country: country).first_or_create!(
+          name: province.name
+        )
+      end
     end
   end
 end
